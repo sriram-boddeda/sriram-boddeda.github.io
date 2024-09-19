90b248c4a17d6c3b628e10d109020f1a90ab8e92
Sriram Boddeda <sriram.b29@outlook.com>
2024-09-19 11:02:34 -0500

    Refactor date display in TimelineDateRange component

--- components/TimelineDateRange.tsx
+++ components/TimelineDateRange.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { format, parseISO } from "date-fns";
+import { CalendarIcon } from "@heroicons/react/24/solid";
 
-const formatDate = (dateString: string) => {
-  if (dateString === "Present") return "Present";
-  const date = parseISO(dateString);
-  return format(date, "MMM yyyy");
+const formatDateRange = (startDate: string, endDate: string) => {
+  if (endDate === "Present")
+    return `${format(parseISO(startDate), "MMM yyyy")} - Present`;
+
+  const start = parseISO(startDate);
+  const end = parseISO(endDate);
+
+  const startMonthYear = format(start, "MMM yyyy");
+  const endMonthYear = format(end, "MMM yyyy");
+
+  if (start.getFullYear() === end.getFullYear()) {
+    if (start.getMonth() === end.getMonth()) {
+      return `${format(start, "MMM d")} - ${format(end, "d yyyy")}`;
+    }
+    return `${startMonthYear} - ${endMonthYear}`;
+  }
+
+  return `${startMonthYear} - ${endMonthYear}`;
 };
 
 interface TimelineDateRangeProps {
@@ -17,9 +32,12 @@ const TimelineDateRange: React.FC<TimelineDateRangeProps> = ({
   endDate,
 }) => {
   return (
-    <div className="md:w-2/12 flex justify-center mb-4 md:mb-0">
-      <div className="text-sm font-semibold text-blue-600 bg-blue-100 py-2 px-4 rounded-full shadow-sm">
-        {formatDate(startDate)} - {formatDate(endDate)}
+    <div className="md:w-2/12 flex justify-center items-center mb-4 md:mb-0">
+      <div className="flex items-center bg-blue-100 py-2 px-4 rounded-full shadow-sm">
+        <CalendarIcon className="w-5 h-5 text-blue-600 mr-2" />
+        <span className="text-sm font-semibold text-blue-600">
+          {formatDateRange(startDate, endDate)}
+        </span>
       </div>
     </div>
   );
