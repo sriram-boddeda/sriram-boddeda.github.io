94e32f007a10c01b770b802c3b7854f2b8cb8b7a
Sriram Boddeda <sriram.b29@outlook.com>
2024-09-19 23:09:33 -0500

    Refactor component styles and remove unused import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { PortfolioData } from "@/types/portfolioTypes";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@
 
 import React from "react";
 import Head from "next/head";
-import Hero from "@/components/Hero";
-import About from "@/components/About";
 import TechnicalSkills from "@/components/TechnicalSkills";
 import Contact from "@/components/Contact";
 import Projects from "@/components/Projects";
@@ -11,10 +9,11 @@ import WorkExperience from "@/components/WorkExperience";
 import Education from "@/components/Education";
 import portfolioData from "@/data/portfolioData.json";
 import { PortfolioData } from "@/types/portfolioTypes";
+import HeroAbout from "@/components/CombinedHero";
 
 const Home: React.FC = () => {
   const {
-    hero,
+    heroAbout,
     about,
     projects,
     experiences,
@@ -32,8 +31,9 @@ const Home: React.FC = () => {
       </Head>
 
       <main>
-        <Hero data={hero} />
-        <About data={about} />
+        <HeroAbout data={heroAbout} />
+        {/* <Hero data={hero} /> */}
+        {/* <About data={about} /> */}
         <Projects data={projects} />
         <WorkExperience data={experiences} />
         <Education data={education} />
@@ -41,10 +41,12 @@ const Home: React.FC = () => {
         <Contact data={contact} />
       </main>
 
-      <footer className="py-4 text-center bg-gray-100 dark:bg-gray-900">
-        <p>
-          &copy; {new Date().getFullYear()} {about.name}. All rights reserved.
-        </p>
+      <footer className="py-6 text-center bg-gray-100 dark:bg-[#121212] border-t border-gray-300 dark:border-gray-700">
+        <div className="container mx-auto px-4">
+          <p className="text-md text-gray-600 dark:text-gray-300">
+            &copy; {new Date().getFullYear()} {about.name}. All rights reserved.
+          </p>
+        </div>
       </footer>
     </>
   );
