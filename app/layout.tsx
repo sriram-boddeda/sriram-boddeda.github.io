391c1e45db37f1715cff6e04d752b733dd07c668
Sriram Boddeda <sriram.b29@outlook.com>
2024-09-19 20:37:48 -0500

    Refactor import statements and update data usage in Home component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { PortfolioData } from "@/types/portfolioTypes";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
--- app/page.tsx
+++ app/page.tsx
@@ -6,37 +6,44 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import TechnicalSkills from "@/components/TechnicalSkills";
 import Contact from "@/components/Contact";
-import portfolioData from "@/data/portfolioData.json";
-import { PortfolioData } from "@/types/portfolioTypes";
 import Projects from "@/components/Projects";
 import WorkExperience from "@/components/WorkExperience";
 import Education from "@/components/Education";
+import portfolioData from "@/data/portfolioData.json";
+import { PortfolioData } from "@/types/portfolioTypes";
 
 const Home: React.FC = () => {
-  const data: PortfolioData = portfolioData;
+  const {
+    hero,
+    about,
+    projects,
+    experiences,
+    education,
+    technicalSkills,
+    contact,
+  } = portfolioData as PortfolioData;
 
   return (
     <>
       <Head>
-        <title>{data.about.name} - Portfolio</title>
-        <meta name="description" content={`Portfolio of ${data.about.name}`} />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <title>{about.name} - Portfolio</title>
+        <meta name="description" content={`Portfolio of ${about.name}`} />
+        {/* Add more meta tags as needed */}
       </Head>
 
       <main>
-        <Hero data={data.hero} />
-        <About data={data.about} />
-        <Projects data={data.projects} />
-        <WorkExperience data={data.experiences} />
-        <Education data={data.education} />
-        <TechnicalSkills data={data.technicalSkills} />
-        <Contact data={data.contact} />
+        <Hero data={hero} />
+        <About data={about} />
+        <Projects data={projects} />
+        <WorkExperience data={experiences} />
+        <Education data={education} />
+        <TechnicalSkills data={technicalSkills} />
+        <Contact data={contact} />
       </main>
 
-      <footer className="py-4 text-center">
+      <footer className="py-4 text-center bg-gray-100 dark:bg-gray-900">
         <p>
-          &copy; {new Date().getFullYear()} {data.about.name}. All rights
-          reserved.
+          &copy; {new Date().getFullYear()} {about.name}. All rights reserved.
         </p>
       </footer>
     </>
