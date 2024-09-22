febf479fd89b7df78314a63e19630f77554fdac0
Sriram Boddeda <sriram.b29@outlook.com>
2024-09-22 18:18:18 -0500

    Refactor component styles to add Java as a skill, update skill icons, and improve the layout in the TechnicalSkills component

--- components/TechnicalSkills.tsx
+++ components/TechnicalSkills.tsx
@@ -1,10 +1,65 @@
 import React from "react";
 import { TechnicalSkillData } from "../types/portfolioTypes";
 import { motion } from "framer-motion";
+import {
+  CSharpIcon,
+  JavaScriptIcon,
+  TypeScriptIcon,
+  PythonIcon,
+  JavaIcon,
+  SolidityIcon,
+  HTMLIcon,
+  CSSIcon,
+  ReactIcon,
+  NextIcon,
+  NodeIcon,
+  DotNetIcon,
+  ShinyIcon,
+  TailwindIcon,
+  GitIcon,
+  DockerIcon,
+  FirebaseIcon,
+  JiraIcon,
+  SQLServerIcon,
+  PostgreSQLIcon,
+  MongoDBIcon,
+  SQLiteIcon,
+  OracleSQLIcon,
+  MSSQLServerIcon,
+} from "@/components/icons/icons";
 
 interface TechnicalSkillsProps {
   data: TechnicalSkillData[];
 }
+
+const iconSize = 24;
+
+const skillIcons: { [key: string]: React.ReactNode } = {
+  "C#": <CSharpIcon size={iconSize} />,
+  Java: <JavaIcon size={iconSize} />,
+  JavaScript: <JavaScriptIcon size={iconSize} />,
+  TypeScript: <TypeScriptIcon size={iconSize} />,
+  Python: <PythonIcon size={iconSize} />,
+  Solidity: <SolidityIcon size={iconSize} />,
+  HTML: <HTMLIcon size={iconSize} />,
+  CSS: <CSSIcon size={iconSize} />,
+  "React.js": <ReactIcon size={iconSize} />,
+  "Next.js": <NextIcon size={iconSize} />,
+  "Node.js": <NodeIcon size={iconSize} />,
+  ".Net": <DotNetIcon size={iconSize} />,
+  Shiny: <ShinyIcon size={iconSize} />,
+  TailwindCSS: <TailwindIcon size={iconSize} />,
+  Git: <GitIcon size={iconSize} />,
+  Docker: <DockerIcon size={iconSize} />,
+  Firebase: <FirebaseIcon size={iconSize} />,
+  Jira: <JiraIcon size={iconSize} />,
+  "MS SQL": <MSSQLServerIcon size={iconSize} />,
+  "Oracle SQL": <OracleSQLIcon size={iconSize} />,
+  MySQL: <SQLServerIcon size={iconSize} />,
+  PostgreSQL: <PostgreSQLIcon size={24} />,
+  MongoDB: <MongoDBIcon size={iconSize} />,
+  SQLite: <SQLiteIcon size={iconSize} />,
+};
 
 const TechnicalSkills: React.FC<TechnicalSkillsProps> = ({ data }) => {
   return (
@@ -26,53 +81,58 @@ const TechnicalSkills: React.FC<TechnicalSkillsProps> = ({ data }) => {
             <span className="text-gray-500">{"//"}</span> Technical Skills
           </h2>
 
+          {/* Skills Grid */}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {data.map((skill, index) => {
-              return (
-                <motion.div
-                  key={skill.id || index}
-                  className="bg-white dark:bg-[#1e1e1e] text-gray-800 dark:text-[#dcdcdc] rounded-lg shadow-lg overflow-hidden  p-4 font-mono"
-                  initial={{ opacity: 0, y: 20 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{ duration: 0.5 }}
-                  whileHover={{
-                    y: -5,
-                    boxShadow: "0 25px 50px -12px rgba(0, 0, 0, 0.25)",
-                  }}
-                >
-                  {/* Skill Category */}
-                  <p className="text-lg font-semibold text-green-600 dark:text-green-400 mb-3">
-                    <span className="text-gray-400 dark:text-gray-500">
-                      {"//"}
-                    </span>{" "}
+            {data.map((skill, index) => (
+              <motion.div
+                key={skill.id || index}
+                className="bg-white dark:bg-[#1e1e1e] text-gray-800 dark:text-[#dcdcdc] rounded-lg shadow-lg overflow-hidden p-6 font-mono"
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+                whileHover={{
+                  y: -5,
+                  boxShadow: "0px -2px 20px rgba(0,0,0,0.25)",
+                }}
+              >
+                {/* Skill Category */}
+                <p className="text-base font-medium text-blue-600 dark:text-blue-400 mb-2">
+                  <span className="text-gray-800 dark:text-green-400">
+                    const
+                  </span>{" "}
+                  <span className="text-purple-600 dark:text-purple-400">
                     {skill.category}
-                  </p>
+                  </span>{" "}
+                  <span className="text-gray-500">=</span>{" "}
+                  <span className="text-gray-500">[</span>
+                </p>
 
-                  {/* Skills Array Declaration */}
-                  <p className="text-base font-medium text-blue-600 dark:text-blue-400 mb-2">
-                    const skills = [
-                  </p>
-
-                  {/* Skills List */}
-                  <div className="pl-4 space-y-1">
-                    {skill.skills.map((item, idx) => (
-                      <p
+                {/* Skill Icons and Names */}
+                <div className="flex flex-wrap gap-y-2 gap-x-1">
+                  {skill.skills.map((item, idx) => {
+                    const IconComponent = skillIcons[item];
+                    return (
+                      <div
                         key={idx}
-                        className="text-base text-gray-700 dark:text-gray-300"
+                        className="flex items-center bg-gray-100 dark:bg-neutral-800 rounded-md p-[4px] shadow-sm "
                       >
-                        &#39;{item}&#39;
+                        {React.isValidElement(IconComponent) &&
+                          React.cloneElement(IconComponent)}
+                        <span className="ml-2 text-gray-700 dark:text-gray-300">
+                          {item}
+                        </span>
                         {idx !== skill.skills.length - 1 ? "," : ""}
-                      </p>
-                    ))}
-                  </div>
+                      </div>
+                    );
+                  })}
+                </div>
 
-                  {/* Array Closing Bracket */}
-                  <p className="text-base font-medium text-blue-600 dark:text-blue-400 mt-2">
-                    ];
-                  </p>
-                </motion.div>
-              );
-            })}
+                {/* Closing bracket */}
+                <p className="text-base font-medium text-blue-600 dark:text-blue-400 mt-2">
+                  <span className="text-gray-500">];</span>
+                </p>
+              </motion.div>
+            ))}
           </div>
         </motion.div>
       </div>
